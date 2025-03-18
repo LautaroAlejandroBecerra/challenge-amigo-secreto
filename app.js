80e48bb26ab2a005a9a0afe264d01b7eb7cf9399
LautaroAlejandroBecerra <becerralautaroalejandro@gmail.com>
2025-03-18 14:07:02 -0300

    segundo commit, funcion orden de la lista

--- app.js
+++ app.js
@@ -10,11 +10,24 @@ function agregarAmigo() {
         amigos.push(nombreAmigo);
         //agregar el valor de nombreAmigo a la lista amigos
         limpiarCaja();
+        //deja el valor de amigo en 0
+        leerLista();
+        //recorre el array y lo deja en 0 para que no repita nombres
     }
 }
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
     //limpia el imput
+}
+
+function leerLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    //en las dos lineas creo una lista en la pagina y vacio la lista amigos para que no repita nombres
+    for (let index = 0; index < amigos.length; index++) {
+       lista.innerHTML += `<li>${amigos[index]}</li>`
+    }
+    //suma un nuevo nombre a la lista
 }
 
